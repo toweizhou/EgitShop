247e9d755b13b6552b8668b2065564a9da8a1d18
ZhouWei <toweizhou@gmail.com>
2015-06-23 11:25:54 +0800

    DAO base

---
+++ src/bean/vo/GoodsVo.java
@@ -0,0 +1,27 @@
+package bean.vo;
+
+public class GoodsVo {
+	private String goodsId;
+	private String goodsName;
+	private Float price;
+	public String getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(String goodsId) {
+		this.goodsId = goodsId;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	
+}
