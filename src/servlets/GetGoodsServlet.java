40a1e6021a8b07eb4e0db7070d6117d4162623c8
ZhouWei <toweizhou@gmail.com>
2015-06-23 11:51:38 +0800

    GetGoodsServlet

---
+++ src/servlets/GetGoodsServlet.java
@@ -0,0 +1,90 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.dao.GoodsDAO;
+
+public class GetGoodsServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public GetGoodsServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		
+		String pageNo = request.getParameter("pageNo");
+		int iPageNo = 1;
+		
+		if(pageNo != null){
+			iPageNo = Integer.parseInt(pageNo);
+		}
+		
+		GoodsDAO dao = new GoodsDAO();
+		ArrayList goodsList = dao.getGoodsByPage(iPageNo);
+		
+		request.setAttribute("goodsList", goodsList);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("goodslist.jsp");
+		rd.forward(request, response);
+
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request,response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
